8c6178ce42bbad023be0fd8107cceaac8910b6a2
Nik Gavalas <gavalnick@gmail.com>
2017-12-26 04:40:52 +0200

    ...

---
+++ src/Blossom/post.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var showdown = require('showdown'),
+	converter = new showdown.Converter({ tables: true });
+
+class Post
+{
+	constructor(title, date, filename, content)
+	{
+		this.title = title;
+		this.date = date;
+		this.filename = filename;
+		this.content = content;
+	}
+
+	getTitle()
+	{
+		return this.title;
+	}
+
+	getDate()
+	{
+		return this.date;
+	}
+
+	getFileName()
+	{
+		return this.filename.replace(/.md/, '.html');
+	}
+
+	getContent()
+	{
+		return converter.makeHtml(this.content);
+	}
+}
+
+module.exports = Post; 
+
